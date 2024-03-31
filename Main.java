b64ea12026f9dcf749c95fdbe634d9bbc812f027
Kirill-Nom <159557878+Kirill-Nom@users.noreply.github.com>
2024-03-31 22:34:38 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,67 +10,80 @@ public class Main {
         char character;
         String[] strings;
         int number = 0;
-        
-        if (input.contains(" + ")) {
-            strings = input.split(" \\+ ");
-            character = '+';
-        } else if (input.contains(" - ")) {
-            strings = input.split(" - ");
-            character = '-';
-        } else if (input.contains(" * ")) {
-            strings = input.split(" \\* ");
-            character = '*';
-        } else if (input.contains(" / ")) {
-            strings = input.split(" / ");
-            character = '/';
-        } else {
-            throw new RuntimeException("Введен некорректный знак действия");
-        }
-        if (character == '-' || character == '+') {
-            if (!strings[1].contains("\"")) {
-                throw new RuntimeException("Строку можно складывать или вычитать из строки");
-            }
-        }
-        if (character == '*' || character == '/') {
-            if (strings[1].contains("\"")) {
-                throw new RuntimeException("Строку можно делить или умножать только на число");
-            }
-            number = Integer.parseInt(strings[1]);
-            if (!(number >= 1 && number <= 10)) {
-                throw new RuntimeException("Вы вышли за диапозон значений");
-            }
-        }
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].replace("\"", "");
-        }
-        if (strings[0].length() > 10) {
-            throw new RuntimeException("Неверный ввод");
-        }
 
-        if (character == '+') {
-            System.out.println(result(strings[0] + strings[1]));
-        } else if (character == '*') {
-            String handledString = "";
-            for (int i = 0; i < number; i++) {
-                handledString += strings[0];
-            }
-            System.out.println(result(handledString));
-        } else if (character == '-') {
-            int index = strings[0].indexOf(strings[1]);
-            if (index == -1) {
-                System.out.println(result(strings[0]));
+            if (input.contains(" + ")) {
+                strings = input.split(" \\+ ");
+                character = '+';
+            } else if (input.contains(" - ")) {
+                strings = input.split(" - ");
+                character = '-';
+            } else if (input.contains(" * ")) {
+                strings = input.split(" \\* ");
+                character = '*';
+            } else if (input.contains(" / ")) {
+                strings = input.split(" / ");
+                character = '/';
             } else {
-                String handledString = strings[0].substring(0, index);
-                handledString += strings[0].substring(index + strings[1].length());
-                System.out.println(result(handledString));
+                throw new RuntimeException("Введен некорректный знак действия");
             }
+            if (character == '-' || character == '+') {
+                if (!strings[1].contains("\"")) {
+                    throw new RuntimeException("Строку можно складывать или вычитать из строки");
+                }
+            }
+            if (character == '*' || character == '/') {
+                number = getNumber(strings);
+            }
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].replace("\"", "");
+            }
+            if (strings[0].length() > 10) {
+                throw new RuntimeException("Неверный ввод");
+            }
+
+            if (character == '+') {
+                System.out.println(result(strings[0] + strings[1]));
+            } else if (character == '*') {
+                extractedFirst(number, strings);
+            } else if (character == '-') {
+                extractedSecond(strings);
+            } else {
+                extractedThird(strings);
+            }
+    }
+    private static int getNumber(String[] strings) {
+        int number;
+        if (strings[1].contains("\"")) {
+            throw new RuntimeException("Строку можно делить или умножать только на число");
+        }
+        number = Integer.parseInt(strings[1]);
+        if (!(number >= 1 && number <= 10)) {
+            throw new RuntimeException("Вы вышли за диапозон значений");
+        }
+        return number;
+    }
+    private static void extractedFirst(int number, String[] strings) {
+        String handledString = "";
+        for (int i = 0; i < number; i++) {
+            handledString += strings[0];
+        }
+        System.out.println(result(handledString));
+    }
+    private static void extractedSecond(String[] strings) {
+        int index = strings[0].indexOf(strings[1]);
+        if (index == -1) {
+            System.out.println(result(strings[0]));
         } else {
-            int newLine = strings[0].length() / Integer.parseInt(strings[1]);
-            String handledString = strings[0].substring(0, newLine);
+            String handledString = strings[0].substring(0, index);
+            handledString += strings[0].substring(index + strings[1].length());
             System.out.println(result(handledString));
         }
     }
-    
+    private static void extractedThird(String[] strings) {
+        int newLine = strings[0].length() / Integer.parseInt(strings[1]);
+        String handledString = strings[0].substring(0, newLine);
+        System.out.println(result(handledString));
+    }
     static String result(String text) {
         if (text.length() > 40) {
             text = text.substring(0, 40);
